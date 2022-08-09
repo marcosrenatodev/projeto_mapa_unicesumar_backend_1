a1b975e1122072ec720279388448bedabbd9bcc9
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2022-08-08 23:33:02 -0300

    adding images and recipes' array

--- dados/receitas.php
+++ dados/receitas.php
@@ -1,13 +1,13 @@
 <?php
 $receitas = array(
-  0 => array('id' => '1', 'nome' => 'PHP', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/arroz_bolinhos.jpg'),
-  1 => array('id' => '2', 'nome' => 'PHP+HTML', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/frango_batatas.jpg'),
-  2 => array('id' => '3', 'nome' => 'PHP+HTML+CSS', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/gravata_pesto.jpg'),
-  3 => array('id' => '4', 'nome' => 'PHP+NODE', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/salada_espeto.jpg'),
-  4 => array('id' => '5', 'nome' => 'PHP+JAVA', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/sand_presunto.jpg'),
-  5 => array('id' => '6', 'nome' => 'PHP+JAVA', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/rucula_frango.jpg'),
-  6 => array('id' => '7', 'nome' => 'PHP+JAVA', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/salada_frango.jpg'),
-  7 => array('id' => '8', 'nome' => 'PHP+JAVA', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/yaki_tofu.jpg'),
+  0 => array('id' => '1', 'nome' => 'Arroz com Bolinhos de Batata', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/arroz_bolinhos.jpg'),
+  1 => array('id' => '2', 'nome' => 'Frango com Batatas', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/frango_batatas.jpg'),
+  2 => array('id' => '3', 'nome' => 'Macarrão Vegano ao Pesto', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/gravata_pesto.jpg'),
+  3 => array('id' => '4', 'nome' => 'Salada no espeto', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/salada_espeto.jpg'),
+  4 => array('id' => '5', 'nome' => 'Sanduíche de presunto', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/sand_presunto.jpg'),
+  5 => array('id' => '6', 'nome' => 'Salada de Frango com Rúcula', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/rucula_frango.jpg'),
+  6 => array('id' => '7', 'nome' => 'Salada de Frango', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/salada_frango.jpg'),
+  7 => array('id' => '8', 'nome' => 'Yakisoba de Tofu', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/yaki_tofu.jpg'),
 
 
 );
