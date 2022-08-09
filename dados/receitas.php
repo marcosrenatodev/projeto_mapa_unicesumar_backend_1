5ca4d375e4026018d22aee0ea46423e0ecd7adc3
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2022-08-08 23:31:45 -0300

    adding images and recipes' array

---
+++ dados/receitas.php
@@ -0,0 +1,13 @@
+<?php
+$receitas = array(
+  0 => array('id' => '1', 'nome' => 'PHP', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/arroz_bolinhos.jpg'),
+  1 => array('id' => '2', 'nome' => 'PHP+HTML', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/frango_batatas.jpg'),
+  2 => array('id' => '3', 'nome' => 'PHP+HTML+CSS', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/gravata_pesto.jpg'),
+  3 => array('id' => '4', 'nome' => 'PHP+NODE', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/salada_espeto.jpg'),
+  4 => array('id' => '5', 'nome' => 'PHP+JAVA', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/sand_presunto.jpg'),
+  5 => array('id' => '6', 'nome' => 'PHP+JAVA', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/rucula_frango.jpg'),
+  6 => array('id' => '7', 'nome' => 'PHP+JAVA', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/salada_frango.jpg'),
+  7 => array('id' => '8', 'nome' => 'PHP+JAVA', 'descricao' => 'Lorem Ipsum has been the industry standard dummy text ever since the', 'imagem' => 'img/yaki_tofu.jpg'),
+
+
+);
